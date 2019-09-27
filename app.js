83cedebc0e20f8674feaecbcf923d2ac2e1a8a77
Yuui Tanabe <yuitanabe@aol.com>
2019-09-28 00:57:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -41,12 +42,7 @@ app.use(function(req, res, next) {
 });
 
 // CORSを許可する
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  next();
-});
+app.use(cors());
 
 // error handler
 app.use(function(err, req, res, next) {
