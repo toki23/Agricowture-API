3601402d7077f8639a0281cc6fd65ed3680db093
Yuui Tanabe <yuitanabe@aol.com>
2019-09-28 02:06:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,12 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET');
-  next();
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
 });
 
 // error handler
