7a7e7fb2ef3b241f3561e769fdfa1d62dd864de7
Yuui Tanabe <yuitanabe@aol.com>
2019-09-28 01:34:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ app.use(function(req, res, next) {
 });
 
 // CORSを許可する
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET');
   next();
 });
 
