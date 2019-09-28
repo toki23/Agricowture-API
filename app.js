180641eb1fb321e764787c0bc15618c429433d08
田邊 悠偉 <tianbianyouwei292@gmail.com>
2019-09-28 10:16:52 +0900

    change

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -39,20 +38,12 @@ mqttClient();
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // CORSを許可する
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  if ('OPTIONS' === req.method) {
-    res.send(200)
-  } else {
-    next()
-  }
-}
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
