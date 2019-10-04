6a7cf2d714b7dee35e8e658bb5576986be650043
Yuui Tanabe <20721486+walker27460@users.noreply.github.com>
2019-10-04 15:08:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -38,9 +38,6 @@ mqttClient();
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
 // error handler
